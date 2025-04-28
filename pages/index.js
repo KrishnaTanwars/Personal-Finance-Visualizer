d763f5aff2a6b0f1cab037f41fc8833cf73e4228
Krishna Tanwar <142567194+KrishnaTanwars@users.noreply.github.com>
2025-04-28 21:31:05 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-export default function Home() {
-    return (
-      <div>
-        <h1>Welcome to Your Personal Finance Visualizer!</h1>
-        <p>This is the homepage of your app.</p>
-      </div>
-    );
-  }
-  
+// pages/index.js
+import React from 'react';
+
+const HomePage = () => {
+  return (
+    <div>
+      <h1>Welcome to Your Personal Finance Visualizer!</h1>
+      <p>This is your app's homepage.</p>
+    </div>
+  );
+};
+
+export default HomePage;
