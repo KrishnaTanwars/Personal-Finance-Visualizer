8a0948aa649c3f0072147cc0fa305ff5be6f6048
Krishna Tanwar <kstanwar0000@gmail.com>
2025-04-28 20:56:56 +0530

    Initial commit

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function About() {
+    return (
+      <div>
+        <h1>About This Project</h1>
+        <p>This is a Personal Finance Visualizer app to track expenses, set budgets, and more.</p>
+      </div>
+    );
+  }
+  
